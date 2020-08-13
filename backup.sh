b71bdf88d5ed007f6f33e8f65030755f8d4937d4
kn <huyhoang8398@gmail.com>
2020-08-13 19:03:09 +0700

    new thinkpad x1 carbon config

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pacman -Qqe | grep -Fvx "$(pacman -Qqm)" > pkglist.txt
+pacman -Qqm > pkglistAUR.txt
+cp -r ~/Pictures .
+cp -r ~/.zshrc ~/.vimrc ~/.xinitrc ~/.xprofile ~/.local/share/icons .
+cp -r ~/.config/nvim .
+cp -r ~/.config/i3 .
+cp -r ~/.config/polybar .
+cp -r /etc/X11/xorg.conf.d .
+cp -r ~/.config/picom .
+cp ~/.config/mimeapps.list .
+cp -r ~/.config/dunst .

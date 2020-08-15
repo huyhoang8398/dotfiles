ae33709a504b354a258c694c44b2147573984a04
kn <huyhoang8398@gmail.com>
2020-08-16 06:16:07 +0700

    Update 2020-08-16

--- polybar/bar_1/launch.sh
+++ polybar/bar_1/launch.sh
@@ -9,5 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top -c ~/.config/polybar/tester.ini &
-polybar mid -c ~/.config/polybar/tester.ini &
+polybar -c ~/.config/polybar/config.ini main &

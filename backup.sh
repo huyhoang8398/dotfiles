21792b8985a3d62a5dbfa56b09343e8dca79c813
kn <huyhoang8398@gmail.com>
2020-11-25 03:51:05 +0100

    Update 2020-11-25

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 pacman -Qqe | grep -Fvx "$(pacman -Qqm)" > pkglist.txt
 pacman -Qqm > pkglistAUR.txt
 cp -r ~/Pictures .
-cp  ~/.zshrc .
-cp ~/.xinitrc .
+cp ~/.zshrc .
+cp ~/.zshenv .
 cp ~/.xprofile .
 cp -r ~/.config/nvim .
 cp -r ~/.config/i3 .

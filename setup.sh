67311df5165a932c695109edf702ee08978ddb89
Kn <huyhoang8398@gmail.com>
2021-04-05 07:38:45 +0200

    Update 2021-04-05

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mv  Pictures ~
+mv .zshrc ~
+mv .zshenv ~
+mv .xprofile ~
+mv nvim ~/.config
+mv i3 ~/.config
+sudo mv xorg.conf.d /etc/X11
+mv picom ~/.config
+mv mimeapps.list ~/.config
+mv dunst ~/.config
+mv i3blocks ~/.config
+mv rofi ~/.config
+mv .themes ~
+mv icons ~/.local/share
+mv .icons ~
+sudo mv fonts /usr/share/fonts
+

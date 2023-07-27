2c1ea181f65916bf83e24eddb28a6f1fc9e7f056
Do Hoang <hdo@agilebio.com>
2023-07-27 14:35:48 +0200

    bufferline show error in filename

--- .config/nvim/lua/kn/plugins/init.lua
+++ .config/nvim/lua/kn/plugins/init.lua
@@ -17,6 +17,12 @@ local default_plugins = {
 		config = function()
 			require("bufferline").setup({
 				options = {
+					diagnostics = "nvim_lsp",
+					diagnostics_update_in_insert = false,
+					diagnostics_indicator = function(count, level, diagnostics_dict, context)
+						local icon = level:match("error") and " " or " "
+						return " " .. icon .. count
+					end,
 					offsets = {
 						{
 							filetype = "NvimTree",

39d8c59c832da16d008306fe69bb79403f13baa5
Do Duy Huy Hoang <huyhoang8398@gmail.com>
2023-08-09 22:43:33 +0200

    Nvimtree folder icon color blue

--- .config/nvim/lua/kn/core/colorscheme.lua
+++ .config/nvim/lua/kn/core/colorscheme.lua
@@ -2,4 +2,12 @@ vim.g.moonflyWinSeparator = 2
 vim.g.moonflyVirtualTextColor = true
 vim.g.moonflyUnderlineMatchParen = true
 vim.g.moonflyCursorColor = true
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "moonfly",
+	callback = function()
+		vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { link = "MoonflyBlue" })
+	end,
+	group = vim.api.nvim_create_augroup("CustomHighlight", {}),
+})
+
 vim.cmd([[colorscheme moonfly]])

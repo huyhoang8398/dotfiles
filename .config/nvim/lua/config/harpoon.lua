e4edec1b787db7bf5d7f23221a7dd92a73cbac07
Do Duy Huy Hoang <hdo@agilebio.com>
2024-07-12 03:16:15 +0200

    Feat: Harpoon

--- .config/nvim/lua/config/harpoon.lua
+++ .config/nvim/lua/config/harpoon.lua
@@ -1,19 +1,9 @@
-local harpoon = require("harpoon")
+local harpoon = require('harpoon')
+harpoon:setup({})
 
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+vim.keymap.set("n", "ha", function() harpoon:list():add() end)
+vim.keymap.set("n", "hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "ha", function()
-	harpoon:list():append()
-end)
-vim.keymap.set("n", "hm", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "hp", function()
-	harpoon:list():prev()
-end)
-vim.keymap.set("n", "hn", function()
-	harpoon:list():next()
-end)
+vim.keymap.set("n", "hp", function() harpoon:list():prev() end)
+vim.keymap.set("n", "hn", function() harpoon:list():next() end)
